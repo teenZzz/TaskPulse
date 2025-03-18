287699d22ad35883b0d2a1ebca45467a0efb0a8a
Брешенков Артём Сергеевич <breshenkov_as@electronxray.com>
2025-03-18 15:23:44 +0300

    Создано основное окно аунтификации, добавлены контролы авторизации и регистрации, создано переключение между ними

---
+++ UserControls/AuthControl.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TaskPulse.UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthControl.xaml
+    /// </summary>
+    public partial class AuthControl : UserControl
+    {
+        public AuthControl()
+        {
+            InitializeComponent();
+        }
+
+        private void textLogin_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            login.Focus();
+        }
+
+        private void login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(login.Text) && login.Text.Length > 0)
+            {
+                textLogin.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textLogin.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            password.Focus();
+        }
+
+        private void password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(password.Password) && password.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+    }
+}
---
+++ ViewModels/AuthWindowViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TaskPulse.ViewModels
+{
+    public class AuthWindowViewModel : INotifyPropertyChanged
+    {
+        public AuthWindowViewModel()
+        {
+            AuthCommand = new RelayCommand(ExecuteAuth, CanExecuteAuth);
+            RegistrCommand = new RelayCommand(ExecuteRegistr, CanExecuteRegistr);
+
+            // Изначально показываем AuthControl
+            IsAuthVisible = true;
+            IsRegistrVisible = false;
+        }
+        private bool _isAuthVisible;
+        private bool _isRegistrVisible;
+
+        // Свойство для видимости AuthControl
+        public bool IsAuthVisible
+        {
+            get => _isAuthVisible;
+            set
+            {
+                if (_isAuthVisible != value)
+                {
+                    _isAuthVisible = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(AuthVisibility)); // Уведомляем об изменении свойства
+                }
+            }
+        }
+
+        // Свойство для видимости RegistrControl
+        public bool IsRegistrVisible
+        {
+            get => _isRegistrVisible;
+            set
+            {
+                if (_isRegistrVisible != value)
+                {
+                    _isRegistrVisible = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(RegistrVisibility)); // Уведомляем об изменении свойства
+                }
+            }
+        }
+
+        // Свойство для конвертации bool в Visibility
+        public Visibility AuthVisibility => IsAuthVisible ? Visibility.Visible : Visibility.Collapsed;
+        public Visibility RegistrVisibility => IsRegistrVisible ? Visibility.Visible : Visibility.Collapsed;
+
+        // Команда для авторизации
+        public ICommand AuthCommand { get; }
+        public ICommand RegistrCommand { get; }
+
+        // Логика для авторизации
+        private void ExecuteAuth(object parameter)
+        {
+            // Логика для авторизации
+            IsAuthVisible = true;
+            IsRegistrVisible = false; // Переход к форме авторизации
+        }
+
+        // Логика для регистрации
+        private void ExecuteRegistr(object parameter)
+        {
+            // Логика для регистрации
+            IsAuthVisible = false;
+            IsRegistrVisible = true; // Переход к форме регистрации
+        }
+
+        // Условие, когда команда "Авторизоваться" может быть выполнена
+        private bool CanExecuteAuth()
+        {
+            // Для примера логика авторизации всегда доступна
+            return true;
+        }
+
+        // Условие, когда команда "Зарегистрироваться" может быть выполнена
+        private bool CanExecuteRegistr()
+        {
+            // Для примера регистрация всегда доступна
+            return true;
+        }
+
+        // Реализация RelayCommand
+        public class RelayCommand : ICommand
+        {
+            private readonly Action<object> _execute;
+            private readonly Func<bool> _canExecute;
+
+            public RelayCommand(Action<object> execute, Func<bool> canExecute)
+            {
+                _execute = execute;
+                _canExecute = canExecute;
+            }
+
+            public bool CanExecute(object parameter)
+            {
+                return _canExecute();
+            }
+
+            public void Execute(object parameter)
+            {
+                _execute(parameter);
+            }
+
+            public event EventHandler CanExecuteChanged;
+        }
+
+        // Реализуем интерфейс INotifyPropertyChanged для обновления UI
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Метод для уведомления об изменении свойства
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
