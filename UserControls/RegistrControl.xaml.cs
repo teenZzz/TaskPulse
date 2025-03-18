d6e44eb2b135955092e8c91da14da1f943eda6d8
Брешенков Артём Сергеевич <breshenkov_as@electronxray.com>
2025-03-18 15:56:17 +0300

    у формы регистрации добавлена возмжность ввода данных

--- UserControls/RegistrControl.xaml.cs
+++ UserControls/RegistrControl.xaml.cs
@@ -24,5 +24,55 @@ namespace TaskPulse.UserControls
         {
             InitializeComponent();
         }
+        private void textLogin_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            login.Focus();
+        }
+
+        private void login_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(login.Text) && login.Text.Length > 0)
+            {
+                textLogin.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textLogin.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            password.Focus();
+        }
+
+        private void password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(password.Password) && password.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            password2.Focus();
+        }
+
+        private void password2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(password2.Password) && password2.Password.Length > 0)
+            {
+                textPassword2.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword2.Visibility = Visibility.Visible;
+            }
+        }
     }
 }
