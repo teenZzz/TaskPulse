80e3eb928a15edbc455141f89ea074fcf1173004
Брешенков Артём Сергеевич <breshenkov_as@electronxray.com>
2025-03-24 12:17:28 +0300

    Начальный этап создания разметки главной страницы

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 using TaskPulse.Classes;
 using TaskPulse.UserControls;
 
@@ -15,7 +16,31 @@ namespace TaskPulse.ViewModels
         public MainWindowViewModel()
         {
             BackCommand = new RelayCommand(ExecuteBack, CanExecuteBack);
+
         }
+
+        private Uri dashboardButtonIcon = new Uri("pack://application:,,,/Resources/dashboard.png");
+        public Uri DashBoardButtonIcon
+        {
+            get => dashboardButtonIcon;
+        }
+        private Uri tasksButtonIcon = new Uri("pack://application:,,,/Resources/tasks.png");
+        public Uri TasksButtonIcon
+        {
+            get => tasksButtonIcon;
+        }
+        private Uri projectsButtonIcon = new Uri("pack://application:,,,/Resources/projects.png");
+        public Uri ProjectsButtonIcon
+        {
+            get => projectsButtonIcon;
+        }
+        private Uri accountButtonIcon = new Uri("pack://application:,,,/Resources/account.png");
+        public Uri AccountButtonIcon
+        {
+            get => accountButtonIcon;
+        }
+
+
         public ICommand BackCommand { get; }
         private void ExecuteBack(object parameter)
         {
