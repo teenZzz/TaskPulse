ba47464d603035a18209861dc81af39c5c3ef06d
EinRoterStern <ppapa9141@gmail.com>
2025-03-31 17:07:10 +0300

    Создано два модальных окна для кнопок "Создать проект" и "Создать задачу"

---
+++ Views/CreateTaskWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TaskPulse.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateTaskWindow.xaml
+    /// </summary>
+    public partial class CreateTaskWindow : Window
+    {
+        public CreateTaskWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
